60cecea0f2183d57f25d8f7655bc18dadab25ce1
Nehal <07nehaljain@gmail.com>
2026-05-25 23:48:56 +0530

    user inprovment

--- src/main/java/com/chat/application/app/Controller/UserController.java
+++ src/main/java/com/chat/application/app/Controller/UserController.java
@@ -2,15 +2,12 @@ package com.chat.application.app.Controller;
 
 import com.chat.application.app.model.User;
 import com.chat.application.app.repository.UserRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "")
+
 public class UserController {
     private final UserRepository userRepository;
 
@@ -22,7 +19,7 @@ public class UserController {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-    @GetMapping("/add-test-users")
+    @PostMapping("/add-test-users")
     public String addUsers() {
         userRepository.save(new User(null, "Rahul"));
         userRepository.save(new User(null, "Yash"));
