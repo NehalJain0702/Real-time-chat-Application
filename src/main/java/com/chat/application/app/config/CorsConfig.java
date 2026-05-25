cc2faa124e514360edd65a6833205a2a667d6995
Nehal <07nehaljain@gmail.com>
2026-05-26 00:16:52 +0530

    cors cofig change

--- src/main/java/com/chat/application/app/config/CorsConfig.java
+++ src/main/java/com/chat/application/app/config/CorsConfig.java
@@ -1,20 +1,19 @@
 package com.chat.application.app.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Value("${app.cors.allowed-origins}")
-    private String allowedOrigins;
-    
+
     @Override
-    public void addCorsMappings (CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins.split(","))
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
