0a148aa80b561579193b38f8e1454862b3027d24
Nehal <07nehaljain@gmail.com>
2026-05-26 23:10:58 +0530

    message

--- src/main/java/com/chat/application/app/Service/UserService.java
+++ src/main/java/com/chat/application/app/Service/UserService.java
@@ -2,7 +2,6 @@ package com.chat.application.app.Service;
 
 import com.chat.application.app.model.User;
 import com.chat.application.app.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,24 +10,18 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository,
-                       PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository) {
 
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> getAllUsers() {
+
         return userRepository.findAll();
     }
 
     public User saveUser(User user) {
-
-        user.setPassword(
-                passwordEncoder.encode(user.getPassword())
-        );
 
         return userRepository.save(user);
     }
